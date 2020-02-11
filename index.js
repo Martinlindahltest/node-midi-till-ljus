88f35680813842272fb0416591cc8f57df195c0d
Anders Lindahl <Anders@Anderss-MacBook-Pro.local>
2020-02-11 15:28:45 +0100

    First Commit

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-var Pusher = require('pusher');
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("hello hello");
+})
+
+function triggPusher(input) {
+    pusher.trigger('my-channel', 'my-event', {
+        "message": input
+    });
+    return("kör pusher-trigg")
+
+}
+
+app.post('/', (req, res) => {
+    console.log(req.body.text);
+    res.send("this is post request");
+    triggPusher("hello World")
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+const Pusher = require('pusher');
 
 var pusher = new Pusher({
     appId: '896495',
@@ -8,6 +43,3 @@ var pusher = new Pusher({
     encrypted: true
 });
 
-pusher.trigger('my-channel', 'my-event', {
-    "message": "hello Daniel"
-});
